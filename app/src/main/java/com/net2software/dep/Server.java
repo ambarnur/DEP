a4a57837e76a0446149c9a2eb9d15aecfe780938
febisandriya <febi.sandiya@gmail.com>
2018-10-21 19:57:54 -0700

    update:final();

--- app/src/main/java/com/net2software/dep/Server.java
+++ app/src/main/java/com/net2software/dep/Server.java
@@ -3,16 +3,16 @@ package com.net2software.dep;
 
 
 public class Server {
-    public static  final String URL = "http://10.100.10.211/api/login";
-    public static  final String URL_REGISTER = "http://10.100.10.211/api/register";
-    public static  final String URL_TEMPAT = "http://10.100.10.211/api/tempat";
-    public static  final String URL_LAPANGAN = "http://10.100.10.211/api/lapangan";
-    public static  final String URL_JADWAL = "http://10.100.10.211/api/jadwal";
-    public static  final String URL_ORDER = "http://10.100.10.211/api/order";
-    public static  final String URL_HARGA = "http://10.100.10.211/api/harga";
-    public static final String URL_IMAGE = "http://10.100.10.211/uploads/gambar/";
-    public static final String URL_UPDATEUSERNAME = "http://10.100.10.211/api/update ";
-    public static final String URL_GETNAME = "http://10.100.10.211/api/getnama";
-    public static final String URL_LISTORDER = "http://10.100.10.211/api/list";
+    public static  final String URL = "http://192.168.43.66/api/login";
+    public static  final String URL_REGISTER = "http://192.168.43.66/api/register";
+    public static  final String URL_TEMPAT = "http://192.168.43.66/api/tempat";
+    public static  final String URL_LAPANGAN = "http://192.168.43.66/api/lapangan";
+    public static  final String URL_JADWAL = "http://192.168.43.66/api/jadwal";
+    public static  final String URL_ORDER = "http://192.168.43.66/api/order";
+    public static  final String URL_HARGA = "http://192.168.43.66/api/harga";
+    public static final String URL_IMAGE = "http://192.168.43.66/uploads/gambar/";
+    public static final String URL_UPDATEUSERNAME = "http://192.168.43.66/api/update ";
+    public static final String URL_GETNAME = "http://192.168.43.66/api/getnama";
+    public static final String URL_LISTORDER = "http://192.168.43.66/api/list";
 
 }
